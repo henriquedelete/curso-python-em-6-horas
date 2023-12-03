4c40fbe3ea3f9fe606a45bd0bac778a71f75bc89
Carlos Henrique Oliveira Silva <oliveirasilvac467@gmail.com>
2023-12-03 11:24:51 -0300

    loops

---
+++ curso-exemple/exemple-for.py
@@ -0,0 +1,40 @@
+# Looping for
+
+for x in range(10):
+    print(x)
+    """
+    aqui ele vai de 0 até 9
+    0,1,2,3,4,5,6,7,8,9
+    """
+print("")
+
+for x in range(1, 10):
+    print(x)
+    """
+    aqui ele vai de 1 até 9
+    1,2,3,4,5,6,7,8,9
+    """
+print("")
+
+for x in range(1, 10, 2):
+    print(x)
+    """
+    aqui ele vai de 1 até 9 só que pulando 2
+    1,3,5,7,9
+    """
+print("")
+
+# os print("") é só pra tipo fica um espaco em branco mesmo.
+
+carros = ["Camaro", "Ranger", "Pajero", "Hilux"]
+
+for carro in carros:
+    print(carro)
+
+"""
+Esta é a saida que resulta desse comando...
+Camaro
+Ranger
+Pajero
+Hilux
+"""
